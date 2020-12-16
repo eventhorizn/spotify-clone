7dce61afc567d07ea478ff741c2cd25272c9094a
Gary Hake <Gary.Hake@waystar.com>
2020-12-16 06:48:23 -0500

    register form, no styling, no  js yet

--- register.php
+++ register.php
@@ -16,10 +16,52 @@
 
             <p>
                 <label for="loginPassword">Password</label>
-                <input id="loginPassword" name="loginPassword" type="password" required>
+                <input id="loginPassword" name="loginPassword" type="password" placeholder="Your password" required>
             </p>
 
             <button type="submit" name="loginButton">Login</button>
+        </form>
+
+
+        <form id="registerForm" action="register.php" method="POST">
+            <h2>Create your free account</h2>
+
+            <p>
+                <label for="username">Username</label>
+                <input id="username" name="username" type="text" placeholder="e.g. bartSimpson" required>
+            </p>
+
+            <p>
+                <label for="firstName">First Name</label>
+                <input id="firstName" name="firstName" type="text" placeholder="e.g. Bart" required>
+            </p>
+
+            <p>
+                <label for="lastName">Last Name</label>
+                <input id="lastName" name="lastName" type="text" placeholder="e.g. Simpson" required>
+            </p>
+
+            <p>
+                <label for="email">Email</label>
+                <input id="email" name="email" type="email" placeholder="e.g. bart@gmail.com" required>
+            </p>
+
+            <p>
+                <label for="email2">Confirm Email</label>
+                <input id="email2" name="email2" type="email" placeholder="e.g. bart@gmail.com" required>
+            </p>
+
+            <p>
+                <label for="password">Password</label>
+                <input id="password" name="password" type="password" placeholder="Your password" required>
+            </p>
+
+            <p>
+                <label for="password2">Confirm Password</label>
+                <input id="password2" name="password2" type="password" placeholder="Your password" required>
+            </p>
+
+            <button type="submit" name="registerButton">Sign Up</button>
         </form>
     </div>
 </body>
