85ba7db1bf71b46aa86f4bfd98ad0c402d926f91
Gary Hake <Gary.Hake@waystar.com>
2020-12-17 13:24:51 -0500

    finished w/ section 2 handling user's submitted data

--- register.php
+++ register.php
@@ -1,10 +1,17 @@
 <?php
     include("includes/classes/Account.php");
+    include("includes/classes/Constants.php");
     $account = new Account();
 
     
     include("includes/handlers/register-handler.php");
     include("includes/handlers/login-handler.php");
+
+    function getInputValue($name) {
+        if (isset($_POST[$name])) {
+            echo $_POST[$name];
+        }
+    }
 ?>
 
 <html>
@@ -36,39 +43,44 @@
             <h2>Create your free account</h2>
 
             <p>
-                <?php echo $account->getError("Your username must be between 5 and 25 characters"); ?>
+                <?php echo $account->getError(Constants::$usernameCharacters); ?>
                 <label for="username">Username</label>
-                <input id="username" name="username" type="text" placeholder="e.g. bartSimpson" required>
+                <input id="username" name="username" type="text" placeholder="e.g. bartSimpson"
+                    value="<?php getInputValue('username') ?>" required>
             </p>
 
             <p>
-                <?php echo $account->getError("Your first name must be between 2 and 25 characters"); ?>
+                <?php echo $account->getError(Constants::$firstNameCharacters); ?>
                 <label for="firstName">First Name</label>
-                <input id="firstName" name="firstName" type="text" placeholder="e.g. Bart" required>
+                <input id="firstName" name="firstName" type="text" placeholder="e.g. Bart"
+                    value="<?php getInputValue('firstName') ?>" required>
             </p>
 
             <p>
-                <?php echo $account->getError("Your last name must be between 2 and 25 characters"); ?>
+                <?php echo $account->getError(Constants::$lastNameCharacters); ?>
                 <label for="lastName">Last Name</label>
-                <input id="lastName" name="lastName" type="text" placeholder="e.g. Simpson" required>
+                <input id="lastName" name="lastName" type="text" placeholder="e.g. Simpson"
+                    value="<?php getInputValue('lastName') ?>" required>
             </p>
 
             <p>
-                <?php echo $account->getError("Your emails do not match"); ?>
-                <?php echo $account->getError("Email is invalid"); ?>
+                <?php echo $account->getError(Constants::$emailsDoNotMatch); ?>
+                <?php echo $account->getError(Constants::$emailInvalid); ?>
                 <label for="email">Email</label>
-                <input id="email" name="email" type="email" placeholder="e.g. bart@gmail.com" required>
+                <input id="email" name="email" type="email" placeholder="e.g. bart@gmail.com"
+                    value="<?php getInputValue('email') ?>" required>
             </p>
 
             <p>
                 <label for="email2">Confirm Email</label>
-                <input id="email2" name="email2" type="email" placeholder="e.g. bart@gmail.com" required>
+                <input id="email2" name="email2" type="email" placeholder="e.g. bart@gmail.com"
+                    value="<?php getInputValue('email2') ?>" required>
             </p>
 
             <p>
-                <?php echo $account->getError("Your passwords do not match"); ?>
-                <?php echo $account->getError("Your password can only contain numbers and letters"); ?>
-                <?php echo $account->getError("Your password must be between 5 and 30 characters"); ?>
+                <?php echo $account->getError(Constants::$passwordsDoNoMatch); ?>
+                <?php echo $account->getError(Constants::$passwordNotAlphanumeric); ?>
+                <?php echo $account->getError(Constants::$passwordCharacters); ?>
                 <label for="password">Password</label>
                 <input id="password" name="password" type="password" placeholder="Your password" required>
             </p>
