7904c214dccb3e0bd7808e56d0a933ecfca1ad4b
Gary Hake <Gary.Hake@waystar.com>
2020-12-17 20:22:09 -0500

    last remaining validation checks

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -47,7 +47,12 @@
                 return;
             }
 
-            //TODO: check if username exists
+            $checkUsernameQuery = mysqli_query($this->con, "SELECT username FROM users WHERE username = '$un'");
+
+            if (mysqli_num_rows($checkUsernameQuery) != 0) {
+                array_push($this->errorArray, Constants::$usernameTaken);
+                return;
+            }
         }
 
         private function validateFirstName($fn) {
@@ -76,7 +81,12 @@
                 return;
             }
 
-            // TODO: check that email hasn't already been used
+            $checkEmailQuery = mysqli_query($this->con, "SELECT email FROM users WHERE email = '$em'");
+
+            if (mysqli_num_rows($checkEmailQuery) != 0) {
+                array_push($this->errorArray, Constants::$emailTaken);
+                return;
+            }
         }
 
         private function validatePasswords($pw, $pw2) {
--- register.php
+++ register.php
@@ -45,6 +45,7 @@
 
             <p>
                 <?php echo $account->getError(Constants::$usernameCharacters); ?>
+                <?php echo $account->getError(Constants::$usernameTaken); ?>
                 <label for="username">Username</label>
                 <input id="username" name="username" type="text" placeholder="e.g. bartSimpson"
                     value="<?php getInputValue('username') ?>" required>
@@ -67,6 +68,7 @@
             <p>
                 <?php echo $account->getError(Constants::$emailsDoNotMatch); ?>
                 <?php echo $account->getError(Constants::$emailInvalid); ?>
+                <?php echo $account->getError(Constants::$emailTaken); ?>
                 <label for="email">Email</label>
                 <input id="email" name="email" type="email" placeholder="e.g. bart@gmail.com"
                     value="<?php getInputValue('email') ?>" required>
