16964d1eac04618bf2ed6bac693524c75f939421
Gary Hake <Gary.Hake@waystar.com>
2020-12-20 17:36:19 -0500

    getting album and artist

--- album.php
+++ album.php
@@ -10,7 +10,12 @@ else {
 $albumQuery = mysqli_query($con, "SELECT * FROM albums WHERE id = '$albumId'");
 $album = mysqli_fetch_array($albumQuery);
 
-echo $album['title'];
+$artistId = $album['artist'];
+$artistQuery = mysqli_query($con, "SELECT * FROM artists WHERE id = '$artistId'");
+$artist = mysqli_fetch_array($artistQuery);
+
+echo $album['title'] . "<br>";
+echo $artist['name'];
 
 ?>
 
