06dbb7747549859253a9d0ad0fd891f5f59b7f14
Gary Hake <Gary.Hake@waystar.com>
2020-12-21 08:12:40 -0500

    play and pause working on test song
    working w/ now playing bar

--- includes/header.php
+++ includes/header.php
@@ -20,6 +20,8 @@ if (isset($_SESSION['userLoggedIn'])) {
     <title>Welcome to Slotify!</title>
 
     <link rel="stylesheet" href="assets/css/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="assets/js/script.js"></script>
 </head>
 
 <body>
--- includes/nowPlayingBarContainer.php
+++ includes/nowPlayingBarContainer.php
@@ -1,77 +1,118 @@
-  <?php?>
-  <div id="nowPlayingBarContainer">
-      <div id="nowPlayingBar">
-          <div id="nowPlayingLeft">
-              <div class="content">
-                  <span class="albumLink">
-                      <img class="albumArtwork" src="assets/images/album-artwork/okc.jpg" alt="">
-                  </span>
+<?php
+$songQuery = mysqli_query($con, "SELECT id FROM songs ORDER BY RAND() LIMIT 10");
 
-                  <div class="trackInfo">
-                      <span class="trackName">
-                          <span>Airbag</span>
-                      </span>
+$resultArray = array();
 
-                      <span class="artistName">
-                          <span>Radiohead</span>
-                      </span>
-                  </div>
-              </div>
-          </div>
+while($row = mysqli_fetch_array($songQuery)) {
+    array_push($resultArray, $row['id']);
+}
 
-          <div id="nowPlayingCenter">
-              <div class="content playerControls">
-                  <div class="buttons">
-                      <button class="controlButton shuffle" title="Shuffle Button">
-                          <img src="assets/images/icons/shuffle.png" alt="Shuffle">
-                      </button>
+$jsonArray = json_encode($resultArray);
 
-                      <button class="controlButton previous" title="Previous Button">
-                          <img src="assets/images/icons/previous.png" alt="Previous">
-                      </button>
+?>
 
-                      <button class="controlButton play" title="Play Button">
-                          <img src="assets/images/icons/play.png" alt="Play">
-                      </button>
+<script>
+$(document).ready(function() {
+    currentPlaylist = <?php echo $jsonArray ?>;
+    audioElement = new Audio();
+    setTrack(currentPlaylist[0], currentPlaylist, false);
+});
 
-                      <button class="controlButton pause" title="Pause Button" style="display: none">
-                          <img src="assets/images/icons/pause.png" alt="Pause">
-                      </button>
+function setTrack(trackId, newPlaylist, play) {
+    audioElement.setTrack("assets/music/radiohead/ok-computer/01 Airbag.mp3");
 
-                      <button class="controlButton next" title="Next Button">
-                          <img src="assets/images/icons/next.png" alt="Next">
-                      </button>
+    if (play) {
+        audioElement.play();
+    }
+}
 
-                      <button class="controlButton repeat" title="Repeat Button">
-                          <img src="assets/images/icons/repeat.png" alt="Repeat">
-                      </button>
-                  </div>
+function playSong() {
+    $(".controlButton.play").hide();
+    $(".controlButton.pause").show();
+    audioElement.play();
+}
 
-                  <div class="playbackBar">
-                      <span class="progressTime current">0.00</span>
-                      <div class="progressBar">
-                          <div class="progressBarBg">
-                              <div class="progress"></div>
-                          </div>
+function pauseSong() {
+    $(".controlButton.play").show();
+    $(".controlButton.pause").hide();
+    audioElement.pause();
+}
+</script>
 
-                      </div>
-                      <span class="progressTime remaining">0.00</span>
-                  </div>
-              </div>
-          </div>
+<div id="nowPlayingBarContainer">
+    <div id="nowPlayingBar">
+        <div id="nowPlayingLeft">
+            <div class="content">
+                <span class="albumLink">
+                    <img class="albumArtwork" src="assets/images/album-artwork/okc.jpg" alt="">
+                </span>
 
-          <div id="nowPlayingRight">
-              <div class="volumeBar">
-                  <button class="controlButton volume" title="Volume Button">
-                      <img src="assets/images/icons/volume.png" alt="Volume">
-                  </button>
+                <div class="trackInfo">
+                    <span class="trackName">
+                        <span>Airbag</span>
+                    </span>
 
-                  <div class="progressBar">
-                      <div class="progressBarBg">
-                          <div class="progress"></div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
-  </div>
+                    <span class="artistName">
+                        <span>Radiohead</span>
+                    </span>
+                </div>
+            </div>
+        </div>
+
+        <div id="nowPlayingCenter">
+            <div class="content playerControls">
+                <div class="buttons">
+                    <button class="controlButton shuffle" title="Shuffle Button">
+                        <img src="assets/images/icons/shuffle.png" alt="Shuffle">
+                    </button>
+
+                    <button class="controlButton previous" title="Previous Button">
+                        <img src="assets/images/icons/previous.png" alt="Previous">
+                    </button>
+
+                    <button class="controlButton play" title="Play Button" onclick="playSong()">
+                        <img src="assets/images/icons/play.png" alt="Play">
+                    </button>
+
+                    <button class="controlButton pause" title="Pause Button" style="display: none"
+                        onclick="pauseSong()">
+                        <img src="assets/images/icons/pause.png" alt="Pause">
+                    </button>
+
+                    <button class="controlButton next" title="Next Button">
+                        <img src="assets/images/icons/next.png" alt="Next">
+                    </button>
+
+                    <button class="controlButton repeat" title="Repeat Button">
+                        <img src="assets/images/icons/repeat.png" alt="Repeat">
+                    </button>
+                </div>
+
+                <div class="playbackBar">
+                    <span class="progressTime current">0.00</span>
+                    <div class="progressBar">
+                        <div class="progressBarBg">
+                            <div class="progress"></div>
+                        </div>
+
+                    </div>
+                    <span class="progressTime remaining">0.00</span>
+                </div>
+            </div>
+        </div>
+
+        <div id="nowPlayingRight">
+            <div class="volumeBar">
+                <button class="controlButton volume" title="Volume Button">
+                    <img src="assets/images/icons/volume.png" alt="Volume">
+                </button>
+
+                <div class="progressBar">
+                    <div class="progressBarBg">
+                        <div class="progress"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
