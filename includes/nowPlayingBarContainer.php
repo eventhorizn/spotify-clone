f09a30c29b4196b5f387833337ef7fc0c8bc51a1
Gary Hake <Gary.Hake@waystar.com>
2020-12-22 06:33:50 -0500

    prevent highlights on bar

--- includes/nowPlayingBarContainer.php
+++ includes/nowPlayingBarContainer.php
@@ -18,6 +18,10 @@ $(document).ready(function() {
     setTrack(currentPlaylist[0], currentPlaylist, false);
     updateVolumeProgressBar(audioElement.audio);
 
+    $('#nowPlayingBarContainer').on("mousedown touchstart mousemove touchmove", function(e) {
+        e.preventDefault();
+    });
+
     $('.playbackBar .progressBar').mousedown(function() {
         mouseDown = true;
     });
