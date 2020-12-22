358dac7823b119f10ac3e96f13a5e63f56950a01
Gary Hake <Gary.Hake@waystar.com>
2020-12-21 21:15:56 -0500

    curent time, remaining time progress bar moves as time progresses

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,37 @@
-var currentPlaylist = [];
-var audioElement;
+let currentPlaylist = [];
+let audioElement;
+
+function formatTime(secondsIn) {
+    const time = Math.round(secondsIn);
+    const minutes = Math.floor(time / 60);
+    const seconds = time - minutes * 60;
+    const extraZero = (seconds < 10) ? "0" : "";
+
+    return `${minutes}:${extraZero}${seconds}`;
+}
+
+function updateTimeProgressBar(audio) {
+    $('.progressTime.current').text(formatTime(audio.currentTime));
+    $('.progressTime.remaining').text(formatTime(audio.duration - audio.currentTime));
+
+    const progress = audio.currentTime / audio.duration * 100;
+    $('.playbackBar .progress').css('width', `${progress}%`);
+}
 
 function Audio() {
     this.currentlyPlaying;
     this.audio = document.createElement('audio');
+
+    this.audio.addEventListener('canplay', function () {
+        const dur = formatTime(this.duration);
+        $('.progressTime.remaining').text(dur);
+    });
+
+    this.audio.addEventListener('timeupdate', function () {
+        if (this.duration) {
+            updateTimeProgressBar(this);
+        }
+    });
 
     this.setTrack = function (track) {
         this.currentlyPlaying = track;
