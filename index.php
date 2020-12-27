ca1dd1e84c927f6a8540470059d96f9b0d1b461d
Gary Hake <Gary.Hake@waystar.com>
2020-12-27 11:26:02 -0500

    missed one

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php include("includes/header.php");?>
 
-<h1 class="pageHeadingBig">You Might Also Like</h1>
+<h1 class="pageHeadingBig disable-select ">You Might Also Like</h1>
 
 <div class="gridViewContainer">
     <?php 
