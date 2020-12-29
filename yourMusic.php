4039828f3f9689f562277c605179d55ff74c2f96
Gary Hake <Gary.Hake@waystar.com>
2020-12-29 09:48:07 -0500

    start of yourMusic location

---
+++ yourMusic.php
@@ -0,0 +1,12 @@
+<?php
+include("includes/includedFiles.php");
+?>
+
+<div class="playlistContainer">
+    <div class="gridviewContainer">
+        <h2>PLAYLISTS</h2>
+        <div class="buttonItems">
+            <button class="button green">NEW PLAYLIST</button>
+        </div>
+    </div>
+</div>
